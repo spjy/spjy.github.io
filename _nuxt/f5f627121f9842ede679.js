d246b9c81a015ae9b98817471380853a5c7682ab
spjy <spjy@hawaii.edu>
2020-05-01 21:48:20 -1000

    fix: navbar

---
+++ _nuxt/f5f627121f9842ede679.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[33],{503:function(n,e,o){"use strict";o.r(e),e.default="---\nheader: Markdown\ndescription: Convenient markup langauge.\n---\n\n\n"}}]);
